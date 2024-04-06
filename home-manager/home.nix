9fc9d094374bfdeec29a2c6db7700f6038d78c95
MDario123 <manuel.dario.oliver@gmail.com>
2024-04-06 19:00:41 -0400

    add mako configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -30,6 +30,32 @@
     };
   };
 
+  services.mako = {
+    enable = true;
+    maxVisible = 5;
+    sort = "-time";
+    layer = "top";
+    anchor = "top-right";
+    font = "FantasqueSansM Nerd Font Mono";
+    backgroundColor = "#1E1E1E";
+    textColor = "#D0F0F0";
+    width = 300;
+    height = 200;
+    margin = "10";
+    padding = "5";
+    borderSize = 2;
+    borderColor = "#F38BA8";
+    borderRadius = 6;
+    progressColor = "over #F38BA855";
+    defaultTimeout = 5000;
+    format = "<b>%s</b>\\n%b";
+    groupBy = "app-name,summary";
+    extraConfig = ''
+      [grouped]
+      format=<b>%s</b>\n%b
+    '';
+  };
+
   # You should not change this value
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
