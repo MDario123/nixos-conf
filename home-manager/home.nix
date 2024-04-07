37038588181d9c8809447ff05f5702c52eba7ddc
MDario123 <manuel.dario.oliver@gmail.com>
2024-04-07 08:42:15 -0400

    mako: set layer to overlay and progressColor a little more opaque

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -34,7 +34,7 @@
     enable = true;
     maxVisible = 5;
     sort = "-time";
-    layer = "top";
+    layer = "overlay";
     anchor = "top-right";
     font = "FantasqueSansM Nerd Font Mono";
     backgroundColor = "#1E1E1E";
@@ -46,7 +46,7 @@
     borderSize = 2;
     borderColor = "#F38BA8";
     borderRadius = 6;
-    progressColor = "over #F38BA855";
+    progressColor = "over #F38BA870";
     defaultTimeout = 5000;
     format = "<b>%s</b>\\n%b";
     groupBy = "app-name,summary";
