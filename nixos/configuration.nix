399f5da236ba196ebf0093fecce5344c1263fc44
MDario123 <manuel.dario.oliver@gmail.com>
2024-04-10 14:00:09 -0400

    finish cleanup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,7 +7,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      # ./terminal.nix
       ./desktops/hyprland.nix
       ./programming.nix
     ];
