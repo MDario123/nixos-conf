b8bfd7e46d5520ce21d2dc7e7a43368cac1d04e3
MDario123 <manuel.dario.oliver@gmail.com>
2024-05-06 00:50:06 -0400

    home-manager: manage kitty

--- home-manager/terminal.nix
+++ home-manager/terminal.nix
@@ -1,6 +1,23 @@
 { config, lib, pkgs, ... }: 
 
 {
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "FantasqueSansM Nerd Font Mono";
+      size = 16;
+      package = with pkgs; (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; });
+    };
+    settings = {
+      tab_bar_min_tabs = 1;
+      tab_bar_edge = "bottom";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
+    };
+    theme = "Catppuccin-Mocha";
+  };
+
   programs.starship = {
     enable = true;
     settings = {
