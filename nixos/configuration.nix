d932554623cd07ca0d37248419d4ef0fbfe56416
MDario123 <manuel.dario.oliver@gmail.com>
2024-05-08 11:29:33 -0400

    nix: add bottom

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -107,6 +107,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    bottom
     cargo
     gnomeExtensions.appindicator
     hakuneko
