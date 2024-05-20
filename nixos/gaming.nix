4276c437c2ceb52b2539c4e2184344005ef04e63
MDario123 <manuel.dario.oliver@gmail.com>
2024-05-20 16:36:47 -0400

    nixos: steam without flatpak

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -5,25 +5,36 @@
   environment.systemPackages = with pkgs; [
     ppsspp
     lutris
+    steam
     steam-run
   ];
 
-  services.flatpak = {
+  hardware.steam-hardware.enable = true;
+  programs.steam = {
     enable = true;
-    remotes = {
-      "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      "flathub-beta" = "https://dl.flathub.org/beta-repo/flathub-beta.flatpakrepo";
-    };
-    packages = [
-      "flathub:app/com.valvesoftware.Steam//stable"
-    ];
-    overrides = {
-      "global" = {
-        filesystems = [
-          "/home/games/SteamLibrary/"
-        ];
-      };
-    };
+    extest.enable = true;
+
+    dedicatedServer.openFirewall = true;
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
   };
+
+  # services.flatpak = {
+  #   enable = true;
+  #   remotes = {
+  #     "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+  #     "flathub-beta" = "https://dl.flathub.org/beta-repo/flathub-beta.flatpakrepo";
+  #   };
+  #   packages = [
+  #     "flathub:app/com.valvesoftware.Steam//stable"
+  #   ];
+  #   overrides = {
+  #     "global" = {
+  #       filesystems = [
+  #         "/home/games/SteamLibrary/"
+  #       ];
+  #     };
+  #   };
+  # };
 }
 
