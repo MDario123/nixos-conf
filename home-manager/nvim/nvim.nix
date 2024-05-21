cb24e8afd135b46339c33a5e1f8ea53ffce6b6cb
MDario123 <manuel.dario.oliver@gmail.com>
2024-05-21 17:05:12 -0400

    home-manager: nvim some more config

--- home-manager/nvim/nvim.nix
+++ home-manager/nvim/nvim.nix
@@ -18,33 +18,39 @@
       # ripgrep
     ];
 
-    # plugins = with pkgs.vimPlugins; [
-    #   {
-    #     plugin = comment-nvim;
-    #     type = "lua";
-    #     config = "require(\"Comment\").setup()";
-    #   }
-    #   {
-    #     plugin = nvim-lspconfig;
-    #     type = "lua";
-    #     config = builtins.readFile ./plugin/lsp.lua;
-    #   }
-    #   neodev-nvim
-    #   nvim-cmp
-    #   telescope-nvim
-    #   telescope-fzf-native-nvim
-    #   cmp_luasnip
-    #   cmp-nvim-lsp
-    #   luasnip
-    #   friendly-snippets
-    #   lualine-nvim
-    #   # nvim-web-icons
-    #   (nvim-treesitter.withPlugins (p: [
-    #     p.tree-sitter-nix
-    #     p.tree-sitter-lua
-    #     p.tree-sitter-rust
-    #     p.tree-sitter-cpp
-    #   ]))
-    # ];
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = comment-nvim;
+        type = "lua";
+        config = "require(\"Comment\").setup()";
+      }
+      {
+        plugin = nvim-lspconfig;
+        type = "lua";
+        config = builtins.readFile ./plugin/lsp.lua;
+      }
+
+      catppuccin-nvim
+      # neodev-nvim
+      nvim-cmp
+      telescope-nvim
+      telescope-fzf-native-nvim
+      # cmp_luasnip
+      # cmp-nvim-lsp
+      # luasnip
+      # friendly-snippets
+      # lualine-nvim
+      # nvim-web-icons
+      (nvim-treesitter.withPlugins (p: [
+        p.tree-sitter-nix
+        p.tree-sitter-lua
+        p.tree-sitter-rust
+        p.tree-sitter-cpp
+      ]))
+    ];
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./options.lua}
+    '';
   };
 }
