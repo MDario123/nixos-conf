825b870ccb77272884f3927d7882f4ef909b7035
MDario123 <manuel.dario.oliver@gmail.com>
2024-05-21 20:46:02 -0400

    home-manager: nvim some more config

---
+++ home-manager/nvim/plugin/telescope.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>fw', '<Cmd> Telescope live_grep <CR>'  , { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>ff', '<Cmd> Telescope find_files <CR>'  , { desc = 'Telescope find files' })
