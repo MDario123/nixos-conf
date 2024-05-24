7c9fb8531e3b090235901b37f5556c9ff9bc540b
MDario123 <manuel.dario.oliver@gmail.com>
2024-05-24 11:03:32 -0400

    home-manager: nvim some more config

--- home-manager/nvim/plugin/barbar.lua
+++ home-manager/nvim/plugin/barbar.lua
@@ -1,1 +1,8 @@
 require('barbar').setup()
+
+-- Change current buffer
+vim.keymap.set('n', '<Tab>', '<Cmd>BufferNext<CR>', { desc = 'Go to next buffer' })
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', { desc = 'Go to previous buffer' })
+
+-- Close current buffer
+vim.keymap.set('n', '<Leader>x', '<Cmd>BufferClose<CR>', { desc = 'Close current buffer' })
