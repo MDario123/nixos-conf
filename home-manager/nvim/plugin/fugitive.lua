8840640ac32329775a013bcd703890655f4bd2a4
MDario123 <manuel.dario.oliver@gmail.com>
2024-06-22 19:21:21 -0400

    nvim: change git push keymap This was done to avoid a conflict

--- home-manager/nvim/plugin/fugitive.lua
+++ home-manager/nvim/plugin/fugitive.lua
@@ -6,4 +6,4 @@ end
 
 map("n", "<leader>s", "<Cmd> G <CR>", "Git [s]tatus")
 map("n", "<leader>hc", "<Cmd> G commit <CR>", "Git [c]ommit")
-map("n", "<leader>hp", "<Cmd> G push <CR>", "Git [p]ush")
+map("n", "<leader>hP", "<Cmd> G push <CR>", "Git [P]ush")
