8d4ebc14ae225f10e7629a21d213fce499070db5
MDario123 <manuel.dario.oliver@gmail.com>
2024-08-09 14:34:34 +0200

    vim fugitive push silent

--- home-manager/nvim/plugin/fugitive.lua
+++ home-manager/nvim/plugin/fugitive.lua
@@ -6,4 +6,4 @@ end
 
 map("n", "<leader>s", "<Cmd> G <CR>", "Git [s]tatus")
 map("n", "<leader>hc", "<Cmd> G commit <CR>", "Git [c]ommit")
-map("n", "<leader>hP", "<Cmd> G! push <CR>", "Git [P]ush")
+vim.keymap.set("n", "<leader>hP", "<Cmd> G! push <CR>", { desc = "Git [P]ush", silent = true })
