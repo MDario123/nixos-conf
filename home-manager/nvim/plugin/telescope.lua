34e365b80c3f64b341442bcf6074e989daa2bb38
MDario123 <manuel.dario.oliver@gmail.com>
2024-09-11 11:00:39 +0200

    feat: add shortcut for :Telescope git_status

--- home-manager/nvim/plugin/telescope.lua
+++ home-manager/nvim/plugin/telescope.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<leader>fw", "<Cmd> Telescope live_grep <CR>", { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>ff", "<Cmd> Telescope find_files <CR>", { desc = "Telescope [f]ind [f]iles" })
 vim.keymap.set("n", "<leader>fh", "<Cmd> Telescope help_tags <CR>", { desc = "Telescope [f]ind [h]elp tags" })
+vim.keymap.set("n", "<leader>fg", "<Cmd> Telescope git_status <CR>", { desc = "Telescope [f]ind [g]it status" })
 vim.keymap.set(
   "n",
   "<leader>fz",
