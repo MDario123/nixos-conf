c64f742e70ee0b40a3a9d0efd3427034f5aa293d
MDario123 <manuel.dario.oliver@gmail.com>
2024-12-13 17:42:08 +0100

    update zen-browser

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
   outputs = { self, nixpkgs, home-manager, nur, aagl, ... } @ inputs:
