34ea37f5186c9bf3d966979f00cae4e06faca310
MDario123 <manuel.dario.oliver@gmail.com>
2024-12-15 23:18:43 +0100

    update from theme to themeFile to get rid of home-manager warning

--- home-manager/graphical/kitty.nix
+++ home-manager/graphical/kitty.nix
@@ -14,7 +14,7 @@
       tab_powerline_style = "slanted";
       tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
     };
-    theme = "Catppuccin-Mocha";
+    themeFile = "Catppuccin-Mocha";
     extraConfig = ''
       map f1 launch --cwd=current
       map f2 close_window
