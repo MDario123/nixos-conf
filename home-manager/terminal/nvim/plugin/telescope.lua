8d9a6a5475b9e5011afcc8843bf5c740b89c1a7b
MDario123 <manuel.dario.oliver@gmail.com>
2024-12-17 01:01:05 +0100

    nvim: more usage of telescope

--- home-manager/terminal/nvim/plugin/telescope.lua
+++ home-manager/terminal/nvim/plugin/telescope.lua
@@ -1,10 +1,25 @@
-vim.keymap.set("n", "<leader>fw", "<Cmd> Telescope live_grep <CR>", { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>ff", "<Cmd> Telescope find_files <CR>", { desc = "Telescope [f]ind [f]iles" })
-vim.keymap.set("n", "<leader>fh", "<Cmd> Telescope help_tags <CR>", { desc = "Telescope [f]ind [h]elp tags" })
-vim.keymap.set("n", "<leader>fg", "<Cmd> Telescope git_status <CR>", { desc = "Telescope [f]ind [g]it status" })
-vim.keymap.set(
-  "n",
-  "<leader>fz",
-  "<Cmd> Telescope current_buffer_fuzzy_find <CR>",
-  { desc = "Telescope find in current buffer" }
-)
+local function map(bind, cmd, desc)
+  vim.keymap.set("n", "<leader>" .. bind, "<Cmd>Telescope " .. cmd .. "<CR>", { desc = desc })
+end
+
+-- Telescope file-related commands
+map("ff", "find_files", "Telescope [f]ind [f]iles")
+map("fb", "buffers", "Telescope [f]ind [b]uffers")
+map("fw", "live_grep", "Telescope live grep")
+map("fz", "current_buffer_fuzzy_find", "Telescope find in current buffer")
+
+-- Telescope Git-related commands
+map("fg", "git_status", "Telescope [f]ind [g]it status")
+map("fB", "git_branches", "Telescope [f]ind [B]ranches")
+
+-- Telescope LSP-related commands
+map("fr", "lsp_references", "Telescope [f]ind [r]eferences")
+map("fs", "lsp_document_symbols", "Telescope [f]ind [s]ymbols")
+map("fo", "lsp_code_actions", "Telescope [f]ind [o]utput")
+map("fi", "lsp_implementations", "Telescope [f]ind [i]mplementations")
+map("fl", "lsp_document_diagnostics", "Telescope [f]ind [l]sp diagnostics")
+
+-- Telescope miscellaneous commands
+map("fh", "help_tags", "Telescope [f]ind [h]elp tags")
+map("fc", "commands", "Telescope [f]ind [c]ommands")
+map("ft", "treesitter", "Telescope [f]ind [t]reesitter")
