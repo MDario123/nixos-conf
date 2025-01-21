e1c907bf03a4b60570be0fc51bfb996a0eb99f93
MDario123 <manuel.dario.oliver@gmail.com>
2025-01-22 00:21:39 +0100

    vim fugitive bindings for git log

--- home-manager/terminal/nvim/plugin/fugitive.lua
+++ home-manager/terminal/nvim/plugin/fugitive.lua
@@ -7,3 +7,16 @@ end
 map("n", "<leader>s", "<Cmd> G <CR>", "Git [s]tatus")
 map("n", "<leader>hc", "<Cmd> G commit <CR>", "Git [c]ommit")
 vim.keymap.set("n", "<leader>hP", "<Cmd> G! push <CR>", { desc = "Git [P]ush", silent = true })
+
+-- Log
+map("n", "<leader>hl", "<Cmd> G log -n 30 <CR>", "Git [l]og")
+map("n", "<leader>hlf", "<Cmd> G log <CR>", "Git [l]og [f]ull")
+-- Graph
+map("n", "<leader>hlg", "<Cmd> G log --graph -n 30 <CR>", "Git [l]og [g]raph")
+map("n", "<leader>hlgf", "<Cmd> G log --graph <CR>", "Git [l]og [g]raph [f]ull")
+-- Oneline
+map("n", "<leader>hlo", "<Cmd> G log --oneline -n 30 <CR>", "Git [l]og [o]neline")
+map("n", "<leader>hlof", "<Cmd> G log --oneline <CR>", "Git [l]og [o]neline [f]ull")
+-- Graph Oneline
+map("n", "<leader>hlgo", "<Cmd> G log --graph --oneline -n 30 <CR>", "Git [l]og [g]raph [o]neline")
+map("n", "<leader>hlgof", "<Cmd> G log --graph --oneline <CR>", "Git [l]og [g]raph [o]neline [f]ull")
