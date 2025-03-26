21a4b3135288c6ee555821c53b882bc5e858008e
MDario123 <manuel.dario.oliver@gmail.com>
2025-03-27 00:40:49 +0100

    configure ollama

--- nixos/activities/programming.nix
+++ nixos/activities/programming.nix
@@ -1,5 +1,15 @@
 { config, lib, pkgs, ... }:
 
+let
+  query_model = (pkgs.writeShellScriptBin "query-ollama" ''
+    #!/usr/bin/env bash
+    jq -Rs --arg model "$1" '{model: $model, prompt: ., stream: false}' | curl -s -X POST http://localhost:11434/api/generate -H "Content-Type: application/json" -d @- | jq -r '.response'
+  '');
+  ai_generate_commit_msg = (pkgs.writeShellScriptBin "ai-gcm" ''
+    #!/usr/bin/env bash
+    { echo '=> git diff --staged'; git diff --staged; echo 'Generate a meaningful, and concise commit message for this changes.'; } | query-ollama "git-expert"
+  '');
+in
 {
   programs.git.enable = true;
 
@@ -16,5 +26,17 @@
     postman
 
     ldtk
+
+    query_model
+    ai_generate_commit_msg
   ];
+
+  services.ollama = {
+    enable = true;
+
+    host = "127.0.0.1";
+    port = 11434;
+
+    acceleration = "cuda";
+  };
 }
