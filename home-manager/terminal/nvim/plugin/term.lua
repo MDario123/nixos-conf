0b00b809eb1a9deb6c6762dc39c8a5d46a2670b4
MDario123 <manuel.dario.oliver@gmail.com>
2025-04-08 16:59:27 +0200

    chore: minor code imprevements to term

--- home-manager/terminal/nvim/plugin/term.lua
+++ home-manager/terminal/nvim/plugin/term.lua
@@ -1,27 +1,25 @@
 local term_buffers = {}
 
-local function toggle_term(term_buffer_name, create_cmd, resume_cmd)
+local function toggle_term(term_buffer_name, create_cmd)
   local term_buffer = term_buffers[term_buffer_name]
 
-  -- Buffer has been deleted, reset the buffer number
-  if term_buffer ~= nil and vim.fn.bufexists(term_buffer) == 0 then
-    term_buffers[term_buffer_name] = nil
-    term_buffer = nil
-  end
-
-  if term_buffer == nil then
-    vim.cmd(create_cmd)
+  if term_buffer == nil or vim.fn.bufexists(term_buffer) == 0 then
+    -- If the buffer doesn't exist, create a new terminal
+    vim.cmd(create_cmd .. "+term")
     term_buffer = vim.fn.bufnr()
     term_buffers[term_buffer_name] = term_buffer
     vim.api.nvim_set_option_value("buflisted", false, { buf = term_buffer })
     vim.cmd("startinsert")
     return
   else
+    -- If the buffer exists, check if it's already open in a window
     local term_window = vim.fn.bufwinnr(term_buffer)
+    -- If it's open, close it
     if term_window > 0 then
       vim.cmd.close({ count = term_window })
+      -- If it's not open, open it in a new window
     else
-      vim.cmd(resume_cmd .. term_buffer)
+      vim.cmd(create_cmd .. "+buffer" .. term_buffer)
       vim.cmd("startinsert")
     end
   end
@@ -29,12 +27,12 @@ end
 
 -- Open a new terminal in a horizontal split
 local function toggle_hterm()
-  return toggle_term("hterm", "botright split +term", "botright split +buffer")
+  return toggle_term("hterm", "botright split")
 end
 
 -- Open a new terminal in a vertical split
 local function toggle_vterm()
-  return toggle_term("vterm", "botright vsplit +term", "botright vsplit +buffer")
+  return toggle_term("vterm", "botright vsplit")
 end
 
 vim.keymap.set({ "n", "t" }, "<A-h>", toggle_hterm, { desc = "Open new horizontal terminal" })
