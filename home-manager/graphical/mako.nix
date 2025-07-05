676c1c724ccb688a975b3b2cf692ea38f241651d
MDario123 <manuel.dario.oliver@gmail.com>
2025-07-05 12:51:21 +0200

    change nixpkgs version to stable

--- home-manager/graphical/mako.nix
+++ home-manager/graphical/mako.nix
@@ -2,29 +2,30 @@
 {
   services.mako = {
     enable = true;
-    maxVisible = 5;
-    output = "eDP-1";
-    sort = "-time";
-    layer = "overlay";
-    anchor = "top-right";
-    font = "FantasqueSansM Nerd Font Mono";
-    backgroundColor = "#1E1E1E";
-    textColor = "#D0F0F0";
-    actions = true;
-    width = 300;
-    height = 200;
-    margin = "10";
-    padding = "5";
-    borderSize = 2;
-    borderColor = "#F38BA8";
-    borderRadius = 6;
-    progressColor = "over #F38BA870";
-    defaultTimeout = 5000;
-    format = "<b>%s</b>\\n%b";
-    groupBy = "app-name,summary";
-    extraConfig = ''
-      [grouped]
-      format=<b>%s</b>\n%b
-    '';
+    settings = {
+      max-visible = 5;
+      output = "eDP-1";
+      sort = "-time";
+      layer = "overlay";
+      anchor = "top-right";
+      font = "FantasqueSansM Nerd Font Mono";
+      background-color = "#1E1E1E";
+      text-color = "#D0F0F0";
+      actions = true;
+      width = 300;
+      height = 200;
+      margin = "10";
+      padding = "5";
+      border-size = 2;
+      border-color = "#F38BA8";
+      border-radius = 6;
+      progress-color = "over #F38BA870";
+      default-timeout = 5000;
+      format = "<b>%s</b>\\n%b";
+      group-by = "app-name,summary";
+      grouped = {
+        format = "<b>%s</b>\\n%b";
+      };
+    };
   };
 }
