ecf6397f64739d91d9d26360d89cd04c436590c3
MDario123 <manuel.dario.oliver@gmail.com>
2025-11-08 12:11:24 +0100

    nvim: bindings to enable/disable copilot

--- home-manager/terminal/nvim/plugin/copilot.lua
+++ home-manager/terminal/nvim/plugin/copilot.lua
@@ -3,3 +3,11 @@ vim.keymap.set("i", "<C-S-Space>", 'copilot#Accept("\\<CR>")', {
   replace_keycodes = false,
 })
 vim.g.copilot_no_tab_map = true
+
+vim.keymap.set("n", "<leader>cd", "<cmd>Copilot disable<CR>", {
+  desc = "Disable Copilot",
+})
+
+vim.keymap.set("n", "<leader>ce", "<cmd>Copilot enable<CR>", {
+  desc = "Enable Copilot",
+})
