957312923b52f5eb99ba363142135e70031f0e1f
MDario123 <manuel.dario.oliver@gmail.com>
2025-12-09 13:37:08 +0100

    improve starship prompt

--- home-manager/terminal/starship.nix
+++ home-manager/terminal/starship.nix
@@ -3,18 +3,34 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = "$character";
-      right_format = "$all";
+      format = "$all$character";
       character = {
         success_symbol = "[=>>](bold green)";
         error_symbol = "[=>>](bold red)";
       };
       add_newline = false;
+
+      aws = {
+        format = "[\\[$symbol$region\\]]($style)";
+        symbol = "☁️ ";
+      };
       git_branch = {
-        disabled = true;
+        format = "[\\[$symbol$branch(:$remote_branch)\\]]($style)";
+        ignore_branches = [
+          "main"
+          "master"
+        ];
       };
       git_status = {
         disabled = false;
+      };
+      nix_shell = {
+        format = "[\\[$symbol$state\\]]($style)";
+        symbol = "❄️ ";
+      };
+      python = {
+        format = "[\\[$symbol$version\\]]($style)";
+        symbol = "🐍 ";
       };
     };
   };
