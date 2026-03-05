9cff772ec2d79473dcf937a854de90420cda8412
MDario123 <manuel.dario.oliver@gmail.com>
2026-03-05 17:47:48 +0100

    add obs-studio

--- nixos/activities/communication.nix
+++ nixos/activities/communication.nix
@@ -1,6 +1,15 @@
-{ config, lib, pkgs, ... }:
+{ pkgs
+, ...
+}:
 
 {
+  programs.obs-studio = {
+    enable = true;
+    package = pkgs.obs-studio.override {
+      cudaSupport = true;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     discord
     telegram-desktop
