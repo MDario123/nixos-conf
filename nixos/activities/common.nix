abe7a54d71cb48800facebf3978d31635ffdd74b
MDario123 <manuel.dario.oliver@gmail.com>
2026-03-05 18:03:20 +0100

    add flatpak and appimage support

--- nixos/activities/common.nix
+++ nixos/activities/common.nix
@@ -1,8 +1,20 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     qmplay2-qt5
     yazi
   ];
+
+  services.flatpak = {
+    enable = true;
+  };
+
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+    package = pkgs.appimage-run.override {
+      extraPkgs = pkgs: [ pkgs.icu ];
+    };
+  };
 }
